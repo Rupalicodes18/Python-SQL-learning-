dc5f4119fca37eb243a48b5376a0a66548b7d90c
Rupali Varma <varmarajesh5728@gmail.com>
2026-04-21 20:21:02 +0530

    Add Hello.py with user info and learning goals

---
+++ Hello.py
@@ -0,0 +1,14 @@
+# Rupali's python Journey.
+# Simple. Clean. Consistent.
+
+name = "Rupali Varma"
+skills = ["Python", "SQL", "Logic"]
+goal = "Building & Learning"
+
+print(f"User: {name}")
+print(f"Tech Stack: {', '.join(skills)}")
+print(f"Current Status: {goal}")
+
+for day in range(1, 8):
+    print(f"Day {day}: Commit. Learn. Repeat.")
+  
