e104fd699e43e80e66211264d0857416603d219a
Rupali Varma <varmarajesh5728@gmail.com>
2026-04-22 23:36:10 +0530

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,1 +1,24 @@
+-- Insert into Customers
+INSERT INTO customers VALUES
+(1, 'Aisha', 'Delhi'),
+(2, 'Riya', 'Mumbai'),
+(3, 'Neha', 'Pune'),
+(4, 'Kavya', 'Surat'),
+(5, 'Rohan', 'Uttar Pradesh');
 
+-- Insert into Products
+INSERT INTO products VALUES
+(101, 'Laptop', 50000),
+(102, 'Phone', 20000),
+(103, 'Headphones', 2000),
+(104, 'Tablet', 30000),
+(105, 'Television',55000);
+
+
+-- Insert into Orders
+INSERT INTO orders VALUES
+(1, 1, 101, 1),
+(2, 2, 102, 2),
+(3, 1, 103, 3),
+(4, 3, 101, 1),
+(5, 4, 104, 1);
