2392a8cd9b4936535338dc5e47130c46264b8686
Rupali Varma <varmarajesh5728@gmail.com>
2026-04-22 23:28:36 +0530

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,1 +1,26 @@
+-- Online Store Database Schema
 
+-- Create Customers Table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(50)
+);
+
+-- Create Products Table
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(50),
+    price INT
+);
+
+-- Create Orders Table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    quantity INT,
+    
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
